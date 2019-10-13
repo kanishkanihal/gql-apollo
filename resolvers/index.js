4cd439f4735ab854ad64f37829396dd3bd573eeb
Kanishka Wickramarathna <kanishkanihal@gmail.com>
2019-10-13 23:10:08 +0530

    Loading dynamic resolvers

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,30 +1,22 @@
-const seller = require("./def/seller");
-const author = require("./def/author");
-const book = require("./def/book");
+const fs = require("fs");
 let resolvers = {
   Query: {},
   Mutation: {}
 };
-
-Object.assign(resolvers.Query, seller.Query);
-Object.assign(resolvers.Mutation, seller.Mutation);
-delete seller.Query;
-delete seller.Mutation;
-Object.assign(resolvers, seller);
-
-Object.assign(resolvers.Query, author.Query);
-Object.assign(resolvers.Mutation, author.Mutation);
-delete author.Query;
-delete author.Mutation;
-Object.assign(resolvers, author);
-
-Object.assign(resolvers.Query, book.Query);
-Object.assign(resolvers.Mutation, book.Mutation);
-delete book.Query;
-delete book.Mutation;
-Object.assign(resolvers, book);
+const files = fs.readdirSync(__dirname.concat("/def"));
+files.map(x => {
+  let y = require(`./def/${x}`);
+  if (y.hasOwnProperty("Query")) {
+    Object.assign(resolvers.Query, y.Query);
+    delete y.Query;
+  }
+  if (y.hasOwnProperty("Mutation")) {
+    Object.assign(resolvers.Mutation, y.Mutation);
+    delete y.Mutation;
+  }
+  Object.assign(resolvers, y);
+});
 
 module.exports = {
   resolvers
 };
-//
